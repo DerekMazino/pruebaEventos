a79666c4203d010f63d888564c710b1bcdf36f39
Camilo <juancmaringcu@gmail.com>
2021-05-31 18:37:31 -0500

    show partakers

--- cliente/src/app/components/partaker-list/partaker-list.component.ts
+++ cliente/src/app/components/partaker-list/partaker-list.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Partaker} from '../../models/partaker';
+import {PartakerServiceService} from '../../services/partaker-service.service'
 @Component({
   selector: 'app-partaker-list',
   templateUrl: './partaker-list.component.html',
   styleUrls: ['./partaker-list.component.css']
 })
 export class PartakerListComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() event: Event;
+  constructor(
+    private partakerService: PartakerServiceService
+  ) { }
+  partakers :Partaker[] = [];
   ngOnInit(): void {
+    this.partakerService.getParkerAll().subscribe(
+      e => this.partakers = e
+    );
   }
 
 }
